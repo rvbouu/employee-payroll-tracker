5cb23cabbf90074ae66f411e1d9368c71ee73878
Vanessa Bou <rvbouu@gmail.com>
2024-04-01 18:17:32 -0500

    Added comments to the script

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ const displayAverageSalary = function(employeesArray) {
   // Finds the average salary and rounds it
   let average = Math.floor(totalSalary/length);
 
+  // Displays message in the console log
   console.log(`The average employee salary between our ${employeesArray.length} employee(s) is $${average}.00`);
 }
 
